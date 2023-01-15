637cca2b9b103c63f74f7825e0ad3959162e3e8b
Oguz Evirgen <evirgenoguz@gmail.com>
2023-01-15 10:27:22 +0300

    bottom nav menu added

--- app/src/main/java/com/example/notesapp/ui/MainActivity.kt
+++ app/src/main/java/com/example/notesapp/ui/MainActivity.kt
@@ -3,6 +3,9 @@ package com.example.notesapp.ui
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.lifecycle.ViewModelProvider
+import androidx.navigation.NavController
+import androidx.navigation.Navigation
+import androidx.navigation.ui.NavigationUI
 import com.example.notesapp.R
 import com.example.notesapp.database.NotesDatabase
 import com.example.notesapp.databinding.ActivityMainBinding
@@ -13,6 +16,8 @@ import com.example.notesapp.viewmodel.providerfactory.NotesViewModelProviderFact
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
+    private lateinit var navController: NavController
+
     val viewModel: NotesViewModel by lazy {
         val database = NotesDatabase.getDatabaseInstance(this)
         val noteRepository = NoteRepository(database)
@@ -25,9 +30,8 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-
-
-
+        navController = Navigation.findNavController(this, R.id.host_fragment)
+        NavigationUI.setupWithNavController(binding.bottomNavMenu, navController)
 
 
     }
